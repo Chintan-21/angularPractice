a3f569d2299573a7eddd3db9d81b5b86cfe10779
Chintan Tajane <chintan21t@gmail.com>
2024-07-02 16:15:45 +0530

    modified app-component & index files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Child1Component } from './components/child1/child1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Child1Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Ng-Practice';
+  title = 'Angular Practice';
+  childMessage: string = 'This is from parent';
+  x!: string;
+  y!: string;
+
+  @ViewChild(Child1Component) pMsg = '';
+
+  constructor() {
+    console.log(this.pMsg);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.pMsg);
+    this.x = (this.pMsg as any).parentMessage;
+    // console.log(this.x);
+  }
+
+  recieveMessage($event: any) {
+    // console.log($event);
+    this.y = $event;
+  }
 }
